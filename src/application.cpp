fbd336e1e8812a3cfec040449a5da44764a0ae2e
Patrick <p.j.hibbs@student.tudelft.nl>
2025-03-22 23:25:33 +0100

    Removed 1 import

--- src/application.cpp
+++ src/application.cpp
@@ -5,7 +5,6 @@ DISABLE_WARNINGS_PUSH()
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
-#include <glm/gtc/matrix_inverse.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 #include <imgui/imgui.h>
