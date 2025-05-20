60033aeed018389b8a6a8ab5635639bffc7ec6dc
Patrick <p.j.hibbs@student.tudelft.nl>
2025-05-20 23:52:22 +0200

    Created a class that creates buffers

---
+++ framework/include/framework/CreateBuffer.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <d3dx12.h>
+#include <d3d12.h>
+#include <wrl/client.h>
+
+template<typename T>
+class CreateBuffer {
+    Microsoft::WRL::ComPtr<ID3D12Resource> buffer;
+
+public:
+    CreateBuffer() = default;
+
+    CreateBuffer(const Microsoft::WRL::ComPtr<ID3D12Device>& device, const int elementCount, const bool isConstantBuffer = false) {
+        auto size = sizeof(T) * elementCount;
+        if(isConstantBuffer) size = (size + 255) & ~255;
+
+        CD3DX12_HEAP_PROPERTIES heapProps(D3D12_HEAP_TYPE_UPLOAD);
+        CD3DX12_RESOURCE_DESC bufferDesc = CD3DX12_RESOURCE_DESC::Buffer(size);
+
+        device->CreateCommittedResource(
+            &heapProps,
+            D3D12_HEAP_FLAG_NONE,
+            &bufferDesc,
+            D3D12_RESOURCE_STATE_GENERIC_READ,
+            nullptr,
+            IID_PPV_ARGS(&buffer)
+        );
+    }
+
+    void copyDataIntoBuffer(std::vector<T> data) {
+        void* dataPtr = nullptr;
+        buffer->Map(0, nullptr, &dataPtr);
+        memcpy(dataPtr, data.data(), data.size() * sizeof(T));
+        buffer->Unmap(0, nullptr);
+
+        dataPtr = nullptr;
+    }
+
+    Microsoft::WRL::ComPtr<ID3D12Resource> getBuffer() {
+        return buffer;
+    }
+};
